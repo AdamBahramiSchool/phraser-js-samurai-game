ef00f5772efdfd5ed6507eb85d085a20ff03b5aa
adambahramischool <afb2@sfu.ca>
2025-12-25 21:54:22 -0800

    init

--- src/game/scenes/Game.js
+++ src/game/scenes/Game.js
@@ -9,6 +9,7 @@ export class Game extends Scene
 
     create ()
     {
+        const { width, height } = this.scale;
         this.cameras.main.setBackgroundColor(0x00ff00);
 
         this.add.image(512, 384, 'background').setAlpha(0.5);
@@ -19,10 +20,51 @@ export class Game extends Scene
             align: 'center'
         }).setOrigin(0.5);
 
-        this.input.once('pointerdown', () => {
+        this.samurai = this.physics.add.image(width/2, height/2, '2dsamurai');
+        this.samurai.setScale(0.2);
+        this.samurai.setCollideWorldBounds(true);
 
-            this.scene.start('GameOver');
+        this.cursors = this.input.keyboard.createCursorKeys();
 
+        // Initialize swinging state
+        this.swinging = false;
+
+        // Sword swing on spacebar
+        this.input.keyboard.on('keydown-SPACE', () => {
+            if (!this.swinging) {
+                this.swinging = true;
+                this.tweens.add({
+                    targets: this.samurai,
+                    angle: 360 * 2,
+                    duration: 100,
+                    yoyo: true,
+                    onComplete: () => { this.swinging = false; }
+                });
+            }
         });
+
+        this.input.once('pointerdown', () => {
+            this.scene.start('GameOver');
+        });
+    }
+
+    update() {
+        const speed = 200;
+        if (!this.samurai) return;
+
+        // Reset velocity every frame
+        this.samurai.setVelocity(0);
+
+        if (this.cursors.left.isDown) {
+            this.samurai.setVelocityX(-speed);
+        } else if (this.cursors.right.isDown) {
+            this.samurai.setVelocityX(speed);
+        }
+
+        if (this.cursors.up.isDown) {
+            this.samurai.setVelocityY(-speed);
+        } else if (this.cursors.down.isDown) {
+            this.samurai.setVelocityY(speed);
+        }
     }
 }
--- src/game/scenes/MainMenu.js
+++ src/game/scenes/MainMenu.js
@@ -9,20 +9,41 @@ export class MainMenu extends Scene
 
     create ()
     {
-        this.add.image(512, 384, 'background');
+        const { width, height } = this.scale;
 
-        this.add.image(512, 300, 'logo');
+        // Center the background image responsively
+        this.add.image(width / 2, height / 2, 'background');
 
-        this.add.text(512, 460, 'Main Menu', {
+        // Scatter samurai and lava images at different locations
+        this.add.image(width * 0.2, height * 0.3, '2dsamurai').setScale(0.3);
+        this.add.image(width * 0.8, height * 0.7, '2dsamurai').setScale(0.3);
+        this.add.image(width * 0.3, height * 0.8, 'lava').setScale(0.3);
+        this.add.image(width * 0.7, height * 0.2, 'lava').setScale(0.3);
+
+        // Centered title
+        this.add.text(width / 2, height / 2 - 100, 'Samurai Hunter', {
+            fontFamily: 'Arial Black', fontSize: 50, color: '#ffffff',
+            stroke: '#CC401D', strokeThickness: 12,
+            align: 'center'
+        }).setOrigin(0.5, 0.5);
+
+        // Centered author
+        this.add.text(width / 2, height / 2, 'Made By: Adam Bahrami', {
+            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 12,
+            align: 'center'
+        }).setOrigin(0.5, 0.5);
+
+        // Centered "Click to Play" below author
+        this.add.text(width / 2, height / 2 + 80, 'Click to Play', {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
-        }).setOrigin(0.5);
+        }).setOrigin(0.5, 0.5);
 
-        this.input.once('pointerdown', () => {
-
+        // Enable click to start game
+        this.input.once('pointerup', () => {
             this.scene.start('Game');
-
         });
     }
 }
